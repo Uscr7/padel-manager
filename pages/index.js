948f381d03a95a8cbe62b7efd1d8c673bb36b439
Uscr7 <t.agaud@gmail.com>
2025-04-14 11:52:21 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-4 text-center">
+      <h1 className="text-2xl font-bold">Bienvenue sur Padel Manager 🎾</h1>
+      <p className="mt-2">L'application arrive bientôt !</p>
+    </main>
+  );
+}
